b492bfc2ad377b0aaa2c769ee9f583acff14e7a8
zihad18 <zihadmaruf397@gmail.com>
2025-12-24 15:21:36 +0600

    f

---
+++ Week2/Day8/insertionTrap.py
@@ -0,0 +1,6 @@
+list1 = [1,2,3,4]
+list2 = list1.copy()
+list1.append(8)
+list2.insert(0, 8)
+print(f"list1 {list1}")
+print(f"list2 {list2}")
---
+++ Week2/Day8/queueBottleneck.py
@@ -0,0 +1,8 @@
+list1 = [1,2,3,4]
+list2 = list1.copy()
+
+list1.pop()
+list2.pop(0)
+
+print(f"list1 {list1}")
+print(f"list2 {list2}")
